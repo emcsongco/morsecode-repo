6f805ca05f4835a69653731427b2a32b7e8777c5
Eva S <evamarie.songco@gmail.com>
2022-09-28 11:45:31 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// export const multiply = (numOne, numTwo) => {
+//     if (typeof numOne !== "number" || typeof numTwo !== "number") {
+//         throw new Error("both parameters must be numbers");
+//         // throw behaves like return
+//         // Error is a built in javascript object like Math
+//     }
+//     return numOne * numTwo;
+// }
+
+// const result = multiply (5,7);
+// console.log(result);
---
+++ index.test.js
@@ -0,0 +1,13 @@
+// import {divide} from "./index";
+
+import {it, expect} from "@jest/globals";
+
+// it ("divides 10 by 2 equal 5", () => {
+//     const result = divide (10,2);
+//     expect(result).toBe(5);
+// });
+
+// it("multiply throws an error is numbers aren't used", () => {
+//     const error = new Error("both parameters must be numbers");
+//     expect(() => multiply("number one",32)).toThrow(error);
+// });
